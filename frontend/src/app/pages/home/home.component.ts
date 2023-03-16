621000aa6f9f4ec08fe6b45983cda0d9406c4566
gisioraelvis <gisioraelvis20@gmail.com>
2023-03-16 12:09:34 +0300

    fixed ngx-pagination

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -5,7 +5,6 @@ import { RouterModule } from '@angular/router';
 import { SearchComponent } from 'src/app/components/search/search.component';
 import { QuestionComponent } from 'src/app/components/question/question.component';
 import { IQuestion } from 'src/app/shared/interfaces/IQuestion';
-import { PaginatorComponent } from 'src/app/components/paginator/paginator.component';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { delay, Observable, of, tap } from 'rxjs';
 import { CommentComponent } from 'src/app/components/comment/comment.component';
@@ -24,7 +23,6 @@ import { SortQuestionsPipe } from 'src/app/shared/pipes/questions-sort.pipe';
     QuestionComponent,
     CommentComponent,
     NgxPaginationModule,
-    PaginatorComponent,
     ProgressSpinnerComponent,
     FilterQuestionsPipe,
     SortQuestionsPipe,
@@ -37,6 +35,8 @@ import { SortQuestionsPipe } from 'src/app/shared/pipes/questions-sort.pipe';
 export class HomeComponent implements OnInit {
   loading: boolean = false;
   page: number = 1;
+  itemsPerPage: number = 10;
+  totalItems?: number;
   questions$?: Observable<IQuestion[]>;
   sortBy: string = 'Newest';
   searchType?: string = 'Keyword';
@@ -57,16 +57,6 @@ export class HomeComponent implements OnInit {
     this.searchTerm = searchTerm;
   }
 
-  // getQuestions() {
-  //   this.loading = true;
-  //   this.questions$ = of(questionFactory.buildList(20)).pipe(
-  //     delay(500), // simulate delay
-  //     tap(() => {
-  //       this.loading = false;
-  //     })
-  //   );
-  // }
-
   searchQuestions(searchTerm: string | undefined | null) {
     this.loading = true;
     // TODO: implement search
@@ -79,11 +69,15 @@ export class HomeComponent implements OnInit {
   }
 
   // TODO: implement pagination take page number and items per page
+  // /questions?page=1&itemsPerPage=10
   getPage($event: number) {
+    this.page = $event;
+    console.log(`Page: ${this.page} Items per page: ${this.itemsPerPage}`);
     this.loading = true;
-    this.questions$ = of(questionFactory.buildList(20)).pipe(
+    this.questions$ = of(questionFactory.buildList(100)).pipe(
       delay(500), // simulate delay
       tap(() => {
+        this.totalItems = 100;
         this.loading = false;
       })
     );
